a9952aae4785df766ac2d06959e45d8611a4e5cb
unknown <katesandersx@gmail.com>
2017-10-08 05:46:13 -0700

    Edited CSS

--- analytics/tut02.py
+++ analytics/tut02.py
@@ -2,15 +2,9 @@ import random
 import string
 import cherrypy
 
-import sys
-sys.path.insert(0, '../analytics')
-from analytics import test
-
-
-
 class StringGenerator(object):
     @cherrypy.expose
-    def pageone(self):
+    def index(self):
 
         return """<html>
         <head>
@@ -28,7 +22,7 @@ class StringGenerator(object):
           document.getElementById('button1').style.color = "#48859D";
           document.getElementById('button1').innerHTML = "<b>RECORD AUDIO</b>";
           document.getElementById('button1').setAttribute( "onClick", "javascript: buttonFunction();" );
-          window.location = "pagetwo";
+          window.location = "pagethree";
         }
 
         </script>
@@ -45,8 +39,9 @@ class StringGenerator(object):
         h1 {
           color: #284A57;
           text-align: center;
-          font-size: 2000%;
+          font-size: 1500%;
           margin-top: 5%;
+          padding-bottom: 4%;
 
         }
 
@@ -79,13 +74,36 @@ class StringGenerator(object):
 """
 
     @cherrypy.expose
-    def pagetwo(self):
-
-        result_list = test()
-
+    def pagethree(self):
         return """<html>
         <head>
         <title>Rover</title>
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type = "text/javascript">
+        </script>
+
+        <script language = "javascript">
+
+        $( document ).ready(function() {
+        $('html, body').animate({ scrollTop: $(document).height() }, 1200);
+        });
+
+        function buttonFunction() {
+          document.getElementById('button1').style.background = "#73A9BF";
+          document.getElementById('button1').style.color = "#EEF4F7";
+          document.getElementById('button1').innerHTML = "<b>STOP RECORDING</b>";
+          document.getElementById('button1').setAttribute( "onClick", "javascript: buttonFunctionTwo();" );
+        }
+        function buttonFunctionTwo() {
+          document.getElementById('button1').style.background = "#FFFFFF";
+          document.getElementById('button1').style.color = "#48859D";
+          document.getElementById('button1').innerHTML = "<b>RECORD AUDIO</b>";
+          document.getElementById('button1').setAttribute( "onClick", "javascript: buttonFunction();" );
+          window.location = "pagethree";
+        }
+
+        </script>
         <style>
         @import url('https://fonts.googleapis.com/css?family=Questrial');
         </style>
@@ -93,14 +111,41 @@ class StringGenerator(object):
 
         body {
           background: linear-gradient(#CBDFE7, #96BFCF);
+          background-attachment: fixed;
           font-family: 'Questrial';
+        }
+
+        h1 {
+          color: #284A57;
+          text-align: center;
+          font-size: 1500%;
+          margin-top: 5%;
+          padding-bottom: 4%;
+
         }
 
         h2 {
           color: #284A57;
           text-align: center;
+          margin-top: -15%;
+          font-size: 300%
+        }
+        h3 {
+          color: #284A57;
+          text-align: center;
           font-size: 300%;
-          margin-top: 5%;
+          margin-top: 20%;
+        }
+
+        button {
+          background-color: #FFFFFF;
+          color: #48859D;
+          border: none;
+          padding: 15px 32px;
+          display: block;
+          margin: 0 auto;
+          font-size: 200%;
+
         }
 
         .art {
@@ -109,7 +154,6 @@ class StringGenerator(object):
           height: 20%;
           margin: 20px 20px 20px 20px;
           margin-bottom: 3%;
-          box-shadow: 2px 2px 10px #38687A;
           margin: 0 auto;
 
         }
@@ -118,22 +162,60 @@ class StringGenerator(object):
           text-align: center;
           padding-top: 2%;
           margin-top: 3%;
-          font-size: 200%;
+          font-size: 150%;
           color: #48859D;
+          padding-bottom: 1%;
+        }
+
+        .author {
+
+            font-size: 80%;
+            text-align: center;
+
+        }
+
+        .a {
+            padding: 20px 20px 20px 20px;
+            margin-top: -1%;
+        }
+
+        .block {
+        margin-bottom: 5%;
+        }
+
+        a {
+        color: inherit; /* blue colors for links too */
+        text-decoration: inherit; /* no underline */
         }
 
         </style>
         </head>
-
         <body id="bod">
-        <h2 id="two">ARTICLES</h2>
-        <div class = article><div class = art><div class = t><b>""" + result_list[0][0] + """</b></div></div></div>
-        <div class = article><div class = art><div class = t><b>""" + result_list[1][0] + """</b></div></div></div>
-        <div class = article><div class = art><div class = t><b>""" + result_list[2][0] + """</b></div></div></div>
-
+        <h1 id="one"><b>ROVER</b></h1>
+        <h2 id="two">It's like Shazam... for scholars!</h2>
+        <button id="button1" onclick="buttonFunction()"><b>RECORD AUDIO</b></button>
+        <h3 id="three">ARTICLES</h3>
+        <div class = block>
+        <div class = article><a href="http://facebook.com"><div class = art><div class = t><b>Title A</b></div><div class = author>AUTHOR NAME</div><div class = a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id.</div></a></div>
+        <div class = article><a href="http://facebook.com"><div class = art><div class = t><b>Title B</b></div><div class = author>AUTHOR NAME</div><div class = a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id.</div></a></div>
+        <div class = article><a href="http://facebook.com"><div class = art><div class = t><b>Title C</b></div><div class = author>AUTHOR NAME</div><div class = a>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id.</div></a></div></div>
         </body>
         </html>
-"""
+        """
+
+        #titlea = variable[0][0]
+        #titleb = variable[1][0]
+        #titlec = variable[2][0]
+        #authora = variable[0][1]
+        #authorb = variable[1][1]
+        #authorc = variable[2][1]
+        #abstracta = variable[0][2]
+        #abstractb = variable[1][2]
+        #abstractc = variable[2][2]
+        #linka = variable[0][3]
+        #linkb = variable[1][3]
+        #linkc = variable[2][3]
+
 
 
 
